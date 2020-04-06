dc3d0b51f1e47ef0de68e5d012132f256c815d6e
Lucas Brendel <lucasbrendel@gmail.com>
2020-04-06 06:38:47 -0500

    Fixing linting errors

--- farmer/ext/output.py
+++ farmer/ext/output.py
@@ -1,11 +1,14 @@
+"""Logging output."""
+
+import logging
 
 import colorama
-import logging
 
 colorama.init()
 
 
 def init(name: str):
+    """Initialize output logging."""
     logging.basicConfig(filename=name, filemode='w', level=logging.INFO,
                         format='%(name)s - %(levelname)s - %(message)s')
 
@@ -17,24 +20,29 @@ def _message(msg: str, color):
 
 
 def message(msg: str, level=0, color=colorama.Fore.WHITE):
+    """Create a basic message. Level Info."""
     _message("{}{}".format("\t" * level, msg), color)
     logging.info(msg)
 
 
 def info(msg: str):
+    """Create info level log."""
     logging.info(msg)
 
 
 def alert(msg: str):
+    """Create warning level log."""
     _message(msg, colorama.Fore.YELLOW)
     logging.warn(msg)
 
 
 def error(msg: str):
+    """Create error level log."""
     _message(msg, colorama.Fore.RED)
     logging.error(msg)
 
 
 def debug(msg: str):
+    """Create a debug log."""
     _message(msg, colorama.Fore.GREEN)
     logging.debug(msg)
