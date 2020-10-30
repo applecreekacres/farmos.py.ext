4db8a84ad7ee9c0f118752100a37fe6d7742abf4
Lucas Brendel <lucasbrendel@gmail.com>
2020-10-30 09:41:44 -0500

    fixing lints

--- farmer/ext/area.py
+++ farmer/ext/area.py
@@ -1,51 +1,105 @@
-
+"""FarmOS Area objects."""
 
 from __future__ import annotations
 
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from farmer.ext.asset import Asset
 from farmer.ext.farmobj import FarmObj
 
 
 class Area(FarmObj):
+    """Area located within a Farm."""
 
     @property
     def assets(self) -> List[Asset]:
-        return self._get_assets(self._assets, Asset) if self._assets else None
+        """Assets assigned to area.
+
+        Returns:
+            Union[List[Asset], None]: [description]
+        """
+        return self._get_assets(self._keys['assets'], Asset)
 
     @property
-    def description(self) ->  str:
-        return self._basic_prop(self._description)
+    def description(self) -> str:
+        """Explanation string of the asset.
+
+        This is not escaped for any formatting that is present.
+
+        Returns:
+            str: Unformatted string.
+        """
+        return FarmObj._basic_prop(self._keys['description'])
 
     @property
     def files(self) -> List:
-        return self._basic_prop(self._files)
+        """Files attached to Area.
+
+        Returns:
+            List: List of file objects.
+        """
+        return FarmObj._basic_prop(self._keys['files'])
 
     @property
     def flags(self) -> List[str]:
-        return self._basic_prop(self._flags)
+        """Flag items assigned.
+
+        Returns:
+            List[str]: Assigned flags by name.
+        """
+        return FarmObj._basic_prop(self._keys['flags'])
 
     @property
     def geofield(self) -> List[Dict]:
-        return self._basic_prop(self._geofield)
+        """Location of area in `Well-Known Text` format.
+
+        Returns:
+            List[Dict]: Described locations.
+        """
+        return FarmObj._basic_prop(self._keys['geofield'])
 
     @property
     def images(self) -> List:
-        return self._basic_prop(self._images)
+        """Image files attached to the area.
+
+        Returns:
+            List: Encoded image files.
+        """
+        return FarmObj._basic_prop(self._keys['images'])
 
     @property
     def parent(self) -> List[Area]:
-        return self._get_areas(self._parent, Area) if self._parent else None
+        """Parent Area of this Area.
+
+        Returns:
+            List[Area]: Generally a single item but stored as a list.
+        """
+        return self._get_areas(self._keys['parent'], Area)
 
     @property
-    def parents_all(self) ->  List:
-        return self._get_areas(self._parents_all, Area) if self._parents_all else None
+    def parents_all(self) -> List[Area]:
+        """All parents.
+
+        Returns:
+            List[Area]: List of parents.
+        """
+        return self._get_areas(self._keys['parents_all'], Area)
 
     @property
-    def tid(self) -> int:
-        return int(self._tid) if self._tid else None
+    def tid(self) -> Union[int, None]:
+        """Taxonomy ID of item.
+
+        Returns:
+            Union[int, None]: Unique ID, none if there is no id but this should
+            not occur.
+        """
+        return int(self._keys['tid']) if self._keys['tid'] else None
 
     @property
     def vocabulary(self) -> Dict:
-        return self._basic_prop(self._vocabulary)
+        """Vocabulary item assigned.
+
+        Returns:
+            Dict: Vocabulary item with id and name.
+        """
+        return FarmObj._basic_prop(self._keys['vocabulary'])
