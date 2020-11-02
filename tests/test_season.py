dd71449d3e96d1c774c47a168c354a7d4702bfd8
Lucas Brendel <lucasbrendel@gmail.com>
2020-11-02 12:59:55 -0600

    adding a basic test

---
+++ tests/test_season.py
@@ -0,0 +1,9 @@
+
+from farmer.ext.farm import Farm
+
+
+def test_season():
+    farm = Farm()
+    seasons = [season for season in farm.seasons]
+    assert len(seasons) > 0
+    assert hasattr(seasons[0], "name")
