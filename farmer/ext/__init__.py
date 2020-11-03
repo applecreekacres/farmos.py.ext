a641a58f0e36ab49d84bbfcf4ce77b3d852653e4
Lucas Brendel <lucasbrendel@gmail.com>
2020-11-03 10:36:09 -0600

    fixing import lengths

--- farmer/ext/__init__.py
+++ farmer/ext/__init__.py
@@ -3,8 +3,10 @@
 import farmer.ext.output as output
 import farmer.ext.prompt as prompt
 from farmer.ext.area import Area
-from farmer.ext.asset import Animal, Asset, Planting, Equipment
+from farmer.ext.asset import Animal, Asset, Equipment, Planting
 from farmer.ext.farm import Farm
-from farmer.ext.log import Harvest, Log, Observation, Seeding, Transplanting, Input, Birth, Activity, Maintenance, Purchase, Sale
+from farmer.ext.log import (Activity, Birth, Harvest, Input, Log, Maintenance,
+                            Observation, Purchase, Sale, Seeding,
+                            Transplanting)
 from farmer.ext.others import Content, Measure, Quantity, User
 from farmer.ext.term import Category, Crop, CropFamily, Season, Term, Unit
