edf88b61784dc377ac8b7a3caf01e133b14a5423
Lucas Brendel <lucasbrendel@gmail.com>
2021-01-22 13:16:44 -0600

    adding weathercloud parser class

---
+++ farmer/weathercloud.py
@@ -0,0 +1,22 @@
+
+import csv
+from typing import Dict, List
+
+# from datetime import datetime
+# import requests
+
+
+class WeatherCloud():
+
+    def __init__(self, database=None) -> None:
+        self._database = database
+        self._data_file = None
+
+    def __enter__(self) -> List[Dict]:
+        data = []
+        self._data_file = open(self._database, 'r')
+        data = csv.DictReader((line.replace('\0', '') for line in self._data_file), delimiter=';')
+        return data
+
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        self._data_file.close()
