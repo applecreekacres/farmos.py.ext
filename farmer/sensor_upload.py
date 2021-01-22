e250f4d2cf993c526fd85d5b0da54195cf81e5fd
Lucas Brendel <lucasbrendel@gmail.com>
2021-01-22 13:16:59 -0600

    using new apis

--- farmer/sensor_upload.py
+++ farmer/sensor_upload.py
@@ -11,6 +11,8 @@ import tkinter as tk
 from tkinter import filedialog
 import requests
 
+from farmer.ext.sensor import Sensor
+from farmer.weathercloud import WeatherCloud
 
 # Attempt 2
 PUB_KEY = "770eba8368bd4e29078cef0294b6ab2e"
@@ -19,10 +21,8 @@ PRV_KEY = "aa8ccf9b7a6ec56412851e041a72ea17"
 # First Station created
 # PUB_KEY = "8f47bee4b063bbf5ee712e246ea893fb"
 # PRV_KEY = "de044e9af43acd486341e00f88e8b37b"
+
 HOST = "https://applecreekacres.farmos.net/"
-SENSOR_URL = "farm/sensor/listener/"
-FULL_URL = "{0}{1}{2}".format(HOST, SENSOR_URL, PUB_KEY)
-# FULL_URL = "{0}{1}{2}?private_key={3}".format(HOST, SENSOR_URL, PUB_KEY, PRV_KEY)
 
 root = tk.Tk()
 root.withdraw()
@@ -46,19 +46,19 @@ first_good_sample_point = datetime(2020, 12, 26, 18, 00, 00)
 
 
 def main():
-    summary = json.loads(requests.get("{}/summary".format(FULL_URL), params={"private_key": PRV_KEY}).text)
-    with open(DATA_FILE, 'r') as data_file:
-        data = csv.DictReader((line.replace('\0', '') for line in data_file), delimiter=';')
+    sensor = Sensor(HOST, PUB_KEY, PRV_KEY)
+    summary = sensor.summary()
+    with WeatherCloud(DATA_FILE) as data:
         for item in data:
             record_date = datetime.strptime(item["Date (America/Chicago)"], "%Y-%m-%d %H:%M:%S")
-            if (not summary or record_date > datetime.fromtimestamp(int(summary['temperature']['last']))) and record_date >= first_good_sample_point:
+            if (not summary or not sensor.get('temperature', record_date, record_date)) and record_date >= first_good_sample_point:
                 record = {
-                    "timestamp": int(record_date.timestamp())
+                    "timestamp": record_date.timestamp()
                 }
                 for key in FIELD_MAPPING:
                     if item[FIELD_MAPPING[key]]:
                         record[key] = float(item[FIELD_MAPPING[key]].replace(',', ''))
-                req = requests.post(FULL_URL, params={"private_key": PRV_KEY}, json=record)
+                sensor.upload(record, False)
                 print("Record for {} uploaded".format(record_date))
                 time.sleep(60)
             else:
