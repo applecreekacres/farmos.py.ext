64090ebf9c9bc22c5b7c10dcf2f2b48cce8f976d
Lucas Brendel <lucasbrendel@gmail.com>
2021-01-25 06:53:18 -0600

    improvements to data handling

--- farmer/sensor_upload.py
+++ farmer/sensor_upload.py
@@ -48,21 +48,29 @@ first_good_sample_point = datetime(2020, 12, 26, 18, 00, 00)
 def main():
     sensor = Sensor(HOST, PUB_KEY, PRV_KEY)
     summary = sensor.summary()
-    with WeatherCloud(DATA_FILE) as data:
-        for item in data:
-            record_date = datetime.strptime(item["Date (America/Chicago)"], "%Y-%m-%d %H:%M:%S")
-            if (not summary or not sensor.get('temperature', record_date, record_date)) and record_date >= first_good_sample_point:
-                record = {
-                    "timestamp": record_date.timestamp()
-                }
-                for key in FIELD_MAPPING:
-                    if item[FIELD_MAPPING[key]]:
-                        record[key] = float(item[FIELD_MAPPING[key]].replace(',', ''))
-                sensor.upload(record, False)
-                print("Record for {} uploaded".format(record_date))
-                time.sleep(60)
-            else:
-                print("Record for {} will not be uploaded".format(record_date))
+    data_uploaded = True
+    start = datetime.now()
+    while data_uploaded:
+        # Reset flag after entering loop
+        data_uploaded = False
+        with WeatherCloud(DATA_FILE) as data:
+            print("Uploading data from {}".format(DATA_FILE))
+            for item in data:
+                record_date = datetime.strptime(item["Date (America/Chicago)"], "%Y-%m-%d %H:%M:%S")
+                if (not summary or not sensor.get(start=record_date, end=record_date)) and record_date >= first_good_sample_point:
+                    record = {
+                        "timestamp": record_date.timestamp()
+                    }
+                    for key in FIELD_MAPPING:
+                        if item[FIELD_MAPPING[key]]:
+                            record[key] = float(item[FIELD_MAPPING[key]].replace(',', ''))
+                    print("Record for {} uploaded".format(record_date))
+                    sensor.upload(record)
+                    data_uploaded = True  # Set flag so that loop will come back and check file again
+                # else:
+                    # print("Record for {} will not be uploaded".format(record_date))
+    total = datetime.now() - start
+    print("Uploaded data in {}".format(total))
 
 
 if __name__ == "__main__":
